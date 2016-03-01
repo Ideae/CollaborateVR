f99a3b250147625f858ea107d04c0729e36f7d22
MHDante <dcamaren@ryerson.ca>
2016-02-29 22:59:04 -0500

    working on paint program for whiteboard

--- CollaborativeVR/Assets/Whiteboard.cs
+++ CollaborativeVR/Assets/Whiteboard.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
 
 public class Whiteboard : MonoBehaviour
@@ -6,17 +7,27 @@ public class Whiteboard : MonoBehaviour
   public Color drawColor = Color.black;
   public int width = 800;
   private int height;
-  private Texture2D texture;
+  private Texture2D boardTexture;
+  public Texture2D brushTexture, shapeTexture;
+
   private Renderer renderer;
   // Use this for initialization
   void Start ()
-	{
-	  height = (int)(width*0.75);
-    texture = new Texture2D(width, height);
+  {
+    float aspect = transform.localScale.y/transform.localScale.x;
+	  height = (int)(width*aspect);//fix
+    boardTexture = new Texture2D(width, height);
     renderer = GetComponent<Renderer>();
-    renderer.material.mainTexture = texture;
+    renderer.material.mainTexture = boardTexture;
 
-    
+    if (brushTexture == null)
+    {
+      brushTexture = (Texture2D)Resources.Load("Textures/brush_256/White1");
+    }
+    if (shapeTexture == null)
+    {
+      shapeTexture = (Texture2D) Resources.Load("WhiteCircle256");
+    }
   }
 	
 	// Update is called once per frame
@@ -46,10 +57,36 @@ public class Whiteboard : MonoBehaviour
 
       if (oldX != x || oldY != y)
       {
-        Debug.Log(hit.point.x + " , " + hit.point.y);
+        //Debug.Log(hit.point.x + " , " + hit.point.y);
 
-        texture.SetPixel(x, y, drawColor);
-        texture.Apply();
+        int minx = Mathf.Max(0, x - shapeTexture.width/2);
+        int miny = Mathf.Max(0, y - shapeTexture.height/2);
+        int maxx = Mathf.Min(boardTexture.width, x + shapeTexture.width/2);
+        int maxy = Mathf.Min(boardTexture.height, y + shapeTexture.height/2);//handle odd case?
+        int w = x - minx, h = y - miny;
+        int minBX = brushTexture.width/2 - w;
+        int minBY = brushTexture.height / 2 - h;
+        int minSX = shapeTexture.width / 2 - w;
+        int minSY = shapeTexture.height / 2 - h;
+        int diffX = maxx - minx;
+        int diffY = maxy - miny;
+
+        for (int i = 0; i < diffX; i++)
+        {
+          for (int j = 0; j < diffY; j++)
+          {
+            int xx = i + minx, yy = j + miny;
+            Color oldCol = boardTexture.GetPixel(xx, yy);
+            Color brushCol = brushTexture.GetPixel(i + minBX, j + minBY);
+            Color shapeCol = shapeTexture.GetPixel(i + minSX, j + minSY);
+            Color temp = (Color.white - (shapeCol));
+            Color temp2 = new Color(Math.Min(1f, temp.r + drawColor.r), Math.Min(1f, temp.g + drawColor.g), Math.Min(1f, temp.b + drawColor.b), Math.Min(1f, temp.a + drawColor.a));
+            Color final = temp2 * oldCol; //brushCol * shapeCol
+            boardTexture.SetPixel(xx, yy, final);
+          }
+        }
+        //boardTexture.SetPixel(x, y, drawColor);
+        boardTexture.Apply();
 
         oldX = x;
         oldY = y;
