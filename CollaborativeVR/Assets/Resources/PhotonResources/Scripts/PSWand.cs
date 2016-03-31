744fddf4f613e6a78e230c284a3724bc8cf8be52
Zack Harris <zack.tibia@gmail.com>
2016-03-31 00:44:39 -0400

    2 ps moves -by dante

--- CollaborativeVR/Assets/Resources/PhotonResources/Scripts/PSWand.cs
+++ CollaborativeVR/Assets/Resources/PhotonResources/Scripts/PSWand.cs
@@ -80,8 +80,8 @@ public class PSWand : MonoBehaviour
   
   private void Update()
   {
-    //bool seen = controller.IsTracking;
-    //controller.SetRumble(seen ? 0f : 0.5f);
+     bool seen = controller.IsTracking;
+    controller.SetRumble(seen ? 0f : 0.5f);
 
     //if (controller.TriggerValue > triggerThreshold && oldTriggerValue <= triggerThreshold)
     //{
