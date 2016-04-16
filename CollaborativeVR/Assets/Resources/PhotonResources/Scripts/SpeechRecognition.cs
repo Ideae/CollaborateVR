7c2d91ad401e04287eb648b781a66315dabd763b
Zack Harris <zack.tibia@gmail.com>
2016-04-16 06:55:34 -0400

    speech recognition, unity beta 5.4.0b14 now in use in order to use unity speech api

---
+++ CollaborativeVR/Assets/Resources/PhotonResources/Scripts/SpeechRecognition.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+public class SpeechRecognition : MonoBehaviour {
+
+  [SerializeField]
+  private Text m_Hypotheses;
+
+  [SerializeField]
+  private Text m_Recognitions;
+
+  private DictationRecognizer m_DictationRecognizer;
+
+  // Use this for initialization
+  void Start () {
+    m_DictationRecognizer = new DictationRecognizer();
+
+    m_DictationRecognizer.DictationResult += (text, confidence) =>
+    {
+      //Debug.LogFormat("Dictation result: {0}", text);
+      m_Recognitions.text = text + "\n";
+    };
+
+    m_DictationRecognizer.DictationHypothesis += (text) =>
+    {
+      //Debug.LogFormat("Dictation hypothesis: {0}", text);
+      m_Hypotheses.text = text + "\n";
+    };
+
+    m_DictationRecognizer.DictationComplete += (completionCause) =>
+    {
+      if (completionCause != DictationCompletionCause.Complete)
+        Debug.LogErrorFormat("Dictation completed unsuccessfully: {0}.", completionCause);
+    };
+
+    m_DictationRecognizer.DictationError += (error, hresult) =>
+    {
+      Debug.LogErrorFormat("Dictation error: {0}; HResult = {1}.", error, hresult);
+    };
+
+    m_DictationRecognizer.Start();
+  }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
